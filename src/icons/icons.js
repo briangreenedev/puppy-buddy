c3e8d9f62e5a4a84d856b7b5215e2adef541e8fb
briangreenedev <brian@cloversoftware.net>
2020-08-10 16:47:30 -0600

    Created custom icons

---
+++ src/icons/icons.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import CustomIcon from "../components/custom-icon";
+import ReportIcon from '@material-ui/icons/AssessmentOutlined';
+import UserIcon from '@material-ui/icons/Face';
+import ContractIcon from '@material-ui/icons/Description';
+import WaitingIcon from '@material-ui/icons/QueryBuilder';
+import LitterIcon from '@material-ui/icons/GroupWork';
+import AccountIcon from '@material-ui/icons/AccountCircle';
+import SettingsIcon from '@material-ui/icons/Settings';
+import LogoutIcon from '@material-ui/icons/ExitToApp';
+
+export {
+	CustomIcon,
+	ReportIcon,
+	UserIcon,
+	ContractIcon,
+	WaitingIcon,
+	LitterIcon,
+	AccountIcon,
+	SettingsIcon,
+	LogoutIcon
+};
