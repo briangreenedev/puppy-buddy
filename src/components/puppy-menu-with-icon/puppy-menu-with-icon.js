910db26ee7d87457d2215d183431c04d0817b1b4
briangreenedev <brian@cloversoftware.net>
2020-08-17 17:11:05 -0600

    Created puppy menu (with icon) component

---
+++ src/components/puppy-menu-with-icon/puppy-menu-with-icon.js
@@ -0,0 +1,53 @@
+import React, {useState} from 'react';
+import ItemMenuIcon from "@material-ui/icons/MoreVert";
+import {
+	IconButton,
+	MenuItem,
+	Menu
+} from "@material-ui/core";
+
+const PuppyMenuWithIcon = ({puppy, items = []}) => {
+	const [anchorElement, setAnchorElement] = useState(null)
+
+	const handleClickMenu = (e) => {
+		setAnchorElement(e.currentTarget);
+	}
+	const handleCloseMenu = (e) => {
+		setAnchorElement(null);
+	}
+
+	const handleRecordWeight = () => {
+		handleCloseMenu();
+	}
+
+	return (
+		<div>
+			<IconButton
+					aria-haspopup="true"
+					aria-controls={`puppy-menu`}
+					onClick={handleClickMenu}>
+				<ItemMenuIcon />
+			</IconButton>
+			<Menu
+				id={`puppy-menu`}
+				keepMounted
+				anchorEl={anchorElement}
+				open={anchorElement}
+				onClose={handleCloseMenu}
+			>
+				{
+					items.map(item => (
+						<MenuItem onClick={() => {
+							handleCloseMenu();
+							return item.handler;
+						}}>{item.name}</MenuItem>
+					))
+				}
+
+			</Menu>
+		</div>
+
+	);
+};
+
+export default PuppyMenuWithIcon;
